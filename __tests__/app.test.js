539846d0d543c7bd13ed44a19166634fee0db454
Jena Boehm <boehmjena@gmail.com>
2021-08-30 20:55:58 -0700

    failing POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,24 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('demo routes', () => {
+  let agent; 
+
   beforeEach(() => {
+    agent = request.agent(app);
     return setup(pool);
+  });
+
+  const instrument = {
+    instrumentName: 'Piano',
+    instrumentType: 'String, Percussion', 
+    origin: 'Italy', 
+    imageUrl: 'https://kawaius.com/wp-content/uploads/2018/06/Kawai-RX-6-Grand-Piano.jpg'
+  };
+
+  it('creates a new instrument via POST', async() => {
+    const res = await agent
+      .post('/api/v1/instruments')
+      .send(instrument);
+    expect(res.body).toEqual({ ...instrument, id: 1 });
   });
 });
